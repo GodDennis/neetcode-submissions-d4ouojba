77e826232377bd30176616f4f7b9bb0caccfe0de
Dennis Podreckiy <109822506+GodDennis@users.noreply.github.com>
2026-04-04 15:32:47 +0300

    Add: daily-temperatures - submission-4

---
+++ Data Structures & Algorithms/daily-temperatures/submission-4.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        const stack = [];
+        const res = Array(temperatures.length).fill(0)
+        for (let i =0; i<temperatures.length; i++){
+            while (stack && temperatures[i] > temperatures[stack[stack.length - 1]]){
+                const j = stack.pop()
+                res[j] = i-j
+            }
+            stack.push(i)
+        }
+        return res
+    }
+}
