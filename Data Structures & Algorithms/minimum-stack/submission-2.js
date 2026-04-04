65fc66780aa49fc2b43de6d36dad2c30b381e5b0
Dennis Podreckiy <109822506+GodDennis@users.noreply.github.com>
2026-04-04 13:36:25 +0300

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.js
@@ -0,0 +1,44 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val)
+
+        
+        if (this.minStack.length === 0 || this.getMin() >= val){
+            this.minStack.push(val)
+        }
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+       const removed = this.stack.pop()
+
+        if (removed === this.getMin()){
+            this.minStack.pop()
+        }
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length - 1]
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.minStack[this.minStack.length - 1]
+    }
+}
