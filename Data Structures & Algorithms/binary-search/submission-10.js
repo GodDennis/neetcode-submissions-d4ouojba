976a68b1367398b1a3b1a4ed6a1e179918f9bcde
Dennis Podreckiy <109822506+GodDennis@users.noreply.github.com>
2026-04-05 18:14:05 +0300

    Add: binary-search - submission-10

---
+++ Data Structures & Algorithms/binary-search/submission-10.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let l = 0
+        let r = nums.length - 1
+
+        while (r >= l){
+            const mid = Math.floor((l + r) / 2)
+            if (nums[mid] === target){
+                return mid
+            } else if (target > nums[mid]){
+                l = mid + 1
+            } else {
+                r = mid - 1
+            }
+        }
+
+        return -1
+    }
+}
