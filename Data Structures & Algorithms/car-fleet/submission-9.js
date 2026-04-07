5096f08718561b069f1608ad79ae9ef58bba7f59
Dennis Podreckiy <109822506+GodDennis@users.noreply.github.com>
2026-04-07 21:55:40 +0300

    Add: car-fleet - submission-9

---
+++ Data Structures & Algorithms/car-fleet/submission-9.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        const arr = position.map((el,i)=>{
+            return {position: el, speed: speed[i]}
+        }).sort((a,b)=> b.position-a.position )
+
+        let fleet = 0;
+        let prevTime = null
+        arr.forEach((el=>{
+            const h = (target - el.position) / el.speed
+
+            if (prevTime < h){
+            fleet++
+            prevTime = h
+            }
+
+        }))
+
+        return fleet
+    }
+}
