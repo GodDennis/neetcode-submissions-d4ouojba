ca6a9394854552ee098eef0faf860e8438c7d5a1
Dennis Podreckiy <109822506+GodDennis@users.noreply.github.com>
2026-04-18 13:53:35 +0300

    Add: depth-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-1.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+
+    sum(root){
+        if (!root) return 0
+
+            return Math.max(this.sum(root.left),this.sum(root.right)) + 1
+        
+
+    }
+
+    maxDepth(root) {
+        return this.sum(root)
+    }
+}
