fe708cc8936910b03e6c9008d97856e00243dbbc
Dennis Podreckiy <109822506+GodDennis@users.noreply.github.com>
2026-04-19 20:36:16 +0300

    Add: longest-consecutive-sequence - submission-10

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-10.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        let set = new Set(nums)
+
+        let arr = Array.from(set).sort((a,b)=> a-b)
+
+        let stack = []
+        for (let i =0; i<arr.length; i++){
+
+            if (arr[i]+1 !== arr[i+1]) stack.push(1)
+            else {
+            if (!stack.length) stack.push(1)
+
+            stack[stack.length -1] = stack[stack.length - 1] + 1
+            }
+
+        }
+
+        return Number.isFinite(Math.max(...stack)) ? Math.max(...stack) : 0
+    }
+}
